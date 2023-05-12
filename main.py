7eae525492da863b0833552c0da5a284351a1234
Georgij <netbay.georgij@gmail.com>
2023-05-12 19:03:23 +0500

    Рефакторинг

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import random
 
 def code_hamming(input_array):
     output_array = input_array.copy()
-    for hamming_bit_number in _iter_hamming_bits_numbers_for_add(len(output_array)):
-        output_array.insert(hamming_bit_number - 1, 0)
-    for hamming_bit_number in _iter_hamming_bits_numbers_for_add(len(output_array)):
-        output_array[hamming_bit_number-1] = _calc_hamming_bit_for(output_array, hamming_bit_number)
+    for bit_pos in _iter_hamming_bits_positions(len(output_array), increase_array_len=True):
+        output_array.insert(bit_pos, 0)
+    for bit_pos in _iter_hamming_bits_positions(len(output_array), increase_array_len=True):
+        output_array[bit_pos] = _calc_hamming_bit_for(output_array, bit_pos+1)
     return output_array
 
 
-def _iter_hamming_bits_numbers_for_add(array_len):
+def _iter_hamming_bits_positions(array_len, increase_array_len=False):
     hamming_bit_power = 0
     hamming_bit_pos = 2 ** hamming_bit_power
     while hamming_bit_pos < array_len:
-        yield hamming_bit_pos
-        array_len += 1
+        yield hamming_bit_pos - 1
+        if increase_array_len:
+            array_len += 1
         hamming_bit_power += 1
         hamming_bit_pos = 2 ** hamming_bit_power
 
@@ -30,15 +31,12 @@ def _calc_hamming_bit_for(input_array, hamming_bit_number):
 
 def decode_hamming(input_array):
     temp_array = input_array.copy()
-    bits_count = 0
-    i = 0
-    while 2**i < len(temp_array):
-        temp_array[2**i-1] = 0
-        bits_count += 1
-        i += 1
 
-    for hamming_bit_number in _iter_hamming_bits_numbers_for_add(len(temp_array) - bits_count):
-        temp_array[hamming_bit_number - 1] = _calc_hamming_bit_for(temp_array, hamming_bit_number)
+    for bit_pos in _iter_hamming_bits_positions(len(input_array)):
+        temp_array[bit_pos] = 0
+
+    for bit_pos in _iter_hamming_bits_positions(len(temp_array)):
+        temp_array[bit_pos] = _calc_hamming_bit_for(temp_array, bit_pos+1)
 
     diff_bit_position = _calc_dif_positions(input_array, temp_array)
     output_array = _fix_bits(diff_bit_position, input_array)

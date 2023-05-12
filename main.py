a0ba5958b2fa23d53cf870aeee1e9c2091b0d69e
Georgij <netbay.georgij@gmail.com>
2023-05-12 18:51:07 +0500

    Рефакторинг

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 import random
 
 
@@ -30,7 +29,6 @@ def _calc_hamming_bit_for(input_array, hamming_bit_number):
 
 
 def decode_hamming(input_array):
-    output_array = input_array.copy()
     temp_array = input_array.copy()
     bits_count = 0
     i = 0
@@ -42,26 +40,40 @@ def decode_hamming(input_array):
     for hamming_bit_number in _iter_hamming_bits_numbers_for_add(len(temp_array) - bits_count):
         temp_array[hamming_bit_number - 1] = _calc_hamming_bit_for(temp_array, hamming_bit_number)
 
-    diff_bit_position = []
-    i = 0
-    while 2 ** i < len(temp_array):
-        if input_array[2**i-1] != temp_array[2**i-1]:
-            diff_bit_position.append(2**i)
-        i += 1
+    diff_bit_position = _calc_dif_positions(input_array, temp_array)
+    output_array = _fix_bits(diff_bit_position, input_array)
+    output_array = _remove_hamming_bits(output_array)
+    return output_array
+
+
+def _fix_bits(diff_bit_position, input_array):
+    output_array = input_array.copy()
     if len(diff_bit_position):
         bit_with_error = sum(diff_bit_position) - 1
         output_array[bit_with_error] = int(not output_array[bit_with_error])
+    return output_array
 
-    bits_positions = []
+
+def _calc_dif_positions(input_array, temp_array):
+    diff_bit_position = []
     i = 0
-    while 2**i < len(output_array):
-        bits_positions.append(2**i-1)
+    while 2 ** i < len(temp_array):
+        if input_array[2 ** i - 1] != temp_array[2 ** i - 1]:
+            diff_bit_position.append(2 ** i)
         i += 1
-    bits_positions = reversed(bits_positions)
+    return diff_bit_position
+
+
+def _remove_hamming_bits(input_array):
+    output_array = input_array.copy()
+    i = 0
+    bits_positions = []
+    while 2 ** i < len(output_array):
+        bits_positions.insert(0, 2 ** i - 1)
+        i += 1
     for bits_pos in bits_positions:
         del output_array[bits_pos]
     return output_array
-
 
 
 if __name__ == '__main__':
@@ -80,3 +92,5 @@ if __name__ == '__main__':
 
     decoded_array = decode_hamming(arr_with_error)
     print("Decoded array", decoded_array)
+
+    assert src_arr == decoded_array
